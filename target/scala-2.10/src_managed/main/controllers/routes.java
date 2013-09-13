1803ee27feb5eebb21a37b6b42dc31512fc70341
Claudio Leite <claudio.leite@corp.globo.com>
2013-09-13 18:01:02 -0300

    adicionando arquivos iniciais do projeto

---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/Users/claudio.leite/Projetos/ambiente-token/conf/routes
+// @HASH:7146bf5ffec26c2f847f18152bb46ae68389afe6
+// @DATE:Fri Sep 13 17:42:42 BRT 2013
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static class javascript {
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();    
+}   
+public static class ref {
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();    
+} 
+}
+              
